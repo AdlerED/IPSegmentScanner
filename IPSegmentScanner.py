10746642ef780303cb067f6b995ac721873f8a68
Adlered <1101635162@qq.com>
2019-04-09 21:04:25 +0800

    V1.0.1 Add arg '-t' to set timeout
    Can improve more available to discovery IPs

--- IPSegmentScanner.py
+++ IPSegmentScanner.py
@@ -6,11 +6,17 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--ipaddress", help="Set IP and set the scan part as \'0\' (Like 192.0.1.1 will scan 192.[range].1.1)")
 parser.add_argument("-r", "--range", help="Set IP scan part range like\'0-255\'")
+parser.add_argument("-t", "--timeout", help="Set the timeout of per IP scan (Recommend 0)")
 parser.add_argument("-d", "--detial", help="Show the detials when scanning", action='store_true')
 args = parser.parse_args()
 ip = args.ipaddress
 minRange = int(args.range.split("-")[0])
 maxRange = int(args.range.split("-")[1])
+timeout =int(args.timeout)
+if(timeout != 0):
+    timeout = str(timeout*100)
+else:
+    timeout = "1"
 
 list = []
 direct = 3;
@@ -23,7 +29,7 @@ for i in range(0,4):
 if direct == 0:
 	print "Scanning " + args.range + '.' + str(ipArray[1]) + '.' + str(ipArray[2]) + '.' + str(ipArray[3])
 	for i in range(minRange, maxRange+1):
-		sql = 'fping -a -t 1 ' + str(i) + '.' + str(ipArray[1]) + '.' + str(ipArray[2]) + '.' + str(ipArray[3])
+		sql = 'fping -a -t ' + timeout + ' ' + str(i) + '.' + str(ipArray[1]) + '.' + str(ipArray[2]) + '.' + str(ipArray[3])
 		(status, output) = commands.getstatusoutput(sql)
 		if output.find('ICMP') == -1 and output != '':
 			list.append(str(output))
@@ -36,7 +42,7 @@ if direct == 0:
 elif direct == 1:
 	print "Scanning " + str(ipArray[0]) + '.' + args.range + '.' + str(ipArray[2]) + '.' + str(ipArray[3])
 	for i in range(minRange, maxRange+1):
-		sql = 'fping -a -t 1 ' + str(ipArray[0]) + '.' + str(i) + '.' + str(ipArray[2]) + '.' + str(ipArray[3])
+		sql = 'fping -a -t ' + timeout + ' ' + str(ipArray[0]) + '.' + str(i) + '.' + str(ipArray[2]) + '.' + str(ipArray[3])
 		(status, output) = commands.getstatusoutput(sql)
 		if output.find('ICMP') == -1 and output != '':
 			list.append(str(output))
@@ -49,7 +55,7 @@ elif direct == 1:
 elif direct == 2:
 	print "Scanning " + str(ipArray[0]) + '.' + str(ipArray[1]) + '.' + args.range + '.' + str(ipArray[3])
 	for i in range(minRange, maxRange+1):
-		sql = 'fping -a -t 1 ' + str(ipArray[0]) + '.' + str(ipArray[1]) + '.' + str(i) + '.' + str(ipArray[3])
+		sql = 'fping -a -t ' + timeout + ' ' + str(ipArray[0]) + '.' + str(ipArray[1]) + '.' + str(i) + '.' + str(ipArray[3])
 		(status, output) = commands.getstatusoutput(sql)
 		if output.find('ICMP') == -1 and output != '':
 			list.append(str(output))
@@ -62,7 +68,7 @@ elif direct == 2:
 elif direct == 3:
 	print "Scanning " + str(ipArray[0]) + '.' + str(ipArray[1]) + '.' + str(ipArray[2]) + '.' + args.range
 	for i in range(minRange, maxRange+1):
-		sql = 'fping -a -t 1 ' + str(ipArray[0]) + '.' + str(ipArray[1]) + '.' + str(ipArray[2]) + '.' + str(i)
+		sql = 'fping -a -t ' + timeout + ' ' + str(ipArray[0]) + '.' + str(ipArray[1]) + '.' + str(ipArray[2]) + '.' + str(i)
 		(status, output) = commands.getstatusoutput(sql)
 		if output.find('ICMP') == -1 and output != '':
 			list.append(str(output))
